9dc3aa4af04e6c777b89397fa2ade7f3bc0df1ae
theAsbringer <kristya1310@gmail.com>
2022-09-01 13:35:57 +0700

    feat: two-way binding

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  return <div>Приложение работает</div>;
+  const [likes, setLikes] = useState(5);
+  const [value, setValue] = useState("ТЕКСТв ИНПУТЕ");
+
+  function increment() {
+    setLikes(likes + 1);
+  }
+
+  function decrement() {
+    setLikes(likes - 1);
+  }
+
+  return (
+    <div>
+      <h1>{likes}</h1>
+      <h1>{value}</h1>
+      <input
+        type="text"
+        value={value}
+        onChange={(evt) => setValue(evt.target.value)}
+      />
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  );
 }
 
 export default App;
