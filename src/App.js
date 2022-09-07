0339f0c43f0fc6a4f98dc0d7c48a72e1ecda8702
theAsbringer <kristya1310@gmail.com>
2022-09-07 13:16:03 +0700

    feat: rendering lists of posts from initial arrays

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React, { useState } from "react";
-import ClassCounter from "./components/ClassCounter";
-import Counter from "./components/Counter";
+import PostList from "./components/PostList";
+import "./styles/App.css";
 
 function App() {
-  const [value, setValue] = useState("ТЕКСТ");
+  const [posts, setPosts] = useState([
+    { id: 1, title: "Javascript", body: "Description" },
+    { id: 2, title: "Javascript 2", body: "Description" },
+    { id: 3, title: "Javascript 3", body: "Description" },
+  ]);
+
+  const [posts2, setPosts2] = useState([
+    { id: 1, title: "Python", body: "Description" },
+    { id: 2, title: "Python 2", body: "Description" },
+    { id: 3, title: "Python 3", body: "Description" },
+  ]);
 
   return (
-    <div>
-      <ClassCounter />
+    <div className="App">
+      <PostList posts={posts} title="Посты про JS" />
+      <PostList posts={posts2} title="Посты про Python" />
     </div>
   );
 }
