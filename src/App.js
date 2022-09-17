5439927d4d00d930289517161dfa1edee9b1938f
theAsbringer <kristya1310@gmail.com>
2022-09-17 13:16:18 +0700

    feat: navbar with react router

--- src/App.js
+++ src/App.js
@@ -1,91 +1,26 @@
 /* eslint-disable react/jsx-filename-extension */
-import React, { useEffect, useState } from 'react';
-import PostService from './API/PostService';
-import PostFilter from './components/PostFilter';
-import PostForm from './components/PostForm';
-import PostList from './components/PostList';
-import MyButton from './components/UI/button/MyButton';
-import MyLoader from './components/UI/loader/MyLoader';
-import MyModal from './components/UI/modal/MyModal';
-import MyPagination from './components/UI/pagination/MyPagination';
-import useFetching from './hooks/useFetching';
-import usePagination from './hooks/usePagination';
-import { usePosts } from './hooks/usePosts';
+import React from 'react';
 import './styles/App.css';
-import getPagesCount from './utils/pages';
+import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
+import Posts from "./pages/Posts";
+import About from "./pages/About";
 
 function App() {
-  const [posts, setPosts] = useState([]);
-  const [filter, setFilter] = useState({ sort: '', query: '' });
-  const [modal, setModal] = useState(false);
-  const [totalPages, setTotalPages] = useState(0);
-  const [limit, setLimit] = useState(10);
-  const [page, setPage] = useState(1);
-  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-
-  const [fetchPosts, isPostsLoading, postError] = useFetching(
-    // eslint-disable-next-line no-shadow
-    async (limit, page) => {
-      const response = await PostService.getAll(limit, page);
-      setPosts(response.data);
-      const totalCount = response.headers['x-total-count'];
-      setTotalPages(getPagesCount(totalCount, limit));
-    },
-  );
-
-  useEffect(() => {
-    fetchPosts(limit, page);
-  }, []);
-
-  const createPost = (newPost) => {
-    setPosts([...posts, newPost]);
-    setModal(false);
-  };
-
-  const removePost = (post) => {
-    setPosts(posts.filter((item) => item.id !== post.id));
-  };
-
-  const changePage = (p) => {
-    setPage(p);
-    fetchPosts(limit, p);
-  };
 
   return (
     <div className="App">
-      <MyButton style={{ margin: '30px 0' }} onClick={() => setModal(true)}>
-        Создать пост
-      </MyButton>
-      <MyModal visible={modal} setVisible={setModal}>
-        <PostForm create={createPost} />
-      </MyModal>
-      <hr style={{ marginBottom: '15px 0' }} />
-      <PostFilter filter={filter} setFilter={setFilter} />
-      {postError && <h1>Произошла ошибка: {postError}</h1>}
-      {isPostsLoading ? (
-        <div
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-            marginTop: '50px',
-          }}
-        >
-          <MyLoader />
-        </div>
-      ) : (
-        <div>
-          <PostList
-            remove={removePost}
-            posts={sortedAndSearchedPosts}
-            title="Посты про JS"
-          />
-          <MyPagination
-            totalPages={totalPages}
-            page={page}
-            changePage={changePage}
-          />
-        </div>
-      )}
+      <BrowserRouter>
+          <div className="navbar">
+              <div className="navbar__links">
+                  <Link className="navbar__link" to="/about">О сайте</Link>
+                  <Link className="navbar__link" to="/posts">Посты</Link>
+              </div>
+          </div>
+          <Routes>
+              <Route path='/about' element={<About/>}/>
+              <Route path='/posts' element={<Posts/>}/>
+          </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function About() {
+    return (
+        <h1>
+            Это приложение создано для тренировки React
+        </h1>
+    );
+}
+
+export default About;
