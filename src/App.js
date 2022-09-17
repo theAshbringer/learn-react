df85c4126de1a68e54d7e3755192f9d267a0f6db
theAsbringer <kristya1310@gmail.com>
2022-09-17 14:13:11 +0700

    feat: component for routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import './styles/App.css';
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import Posts from "./pages/Posts";
-import About from "./pages/About";
+import { BrowserRouter } from "react-router-dom";
 import MyNavBar from './components/UI/navbar/MyNavBar';
-import Error from './pages/Error';
+import AppRouter from './components/AppRouter';
 
 function App() {
 
@@ -13,12 +11,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <MyNavBar />
-        <Routes>
-          <Route path='/about' element={<About />} />
-          <Route path='/posts' element={<Posts />} />
-          <Route path='/error' element={<Error />} />
-          <Route path='/*' element={<Navigate to="/error" />} />
-        </Routes>
+        <AppRouter />
       </BrowserRouter>
     </div>
   );
---
+++ src/components/AppRouter.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import About from '../pages/About'
+import Error from '../pages/Error'
+import Posts from '../pages/Posts'
+
+function AppRouter() {
+  return (
+    <div>
+      <Routes>
+        <Route path='/about' element={<About />} />
+        <Route path='/posts' element={<Posts />} />
+        <Route path='/error' element={<Error />} />
+        <Route path='/*' element={<Navigate to="/error" />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default AppRouter
